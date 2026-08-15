8f3d640e81a2a5b36b70e6f031559d6b57459873
Yash Sathe <yashsathe1357@gmail.com>
2026-08-15 14:32:24 +0530

    Displayed the products in Homepage and the control the quatityToggle button

--- homeProductCards.js
+++ homeProductCards.js
@@ -1,3 +1,40 @@
-export const showProductContainer = (products) =>{
-    
-}
+import { homeQuantityToggle } from "./quantityToggle.js";
+const productContainer = document.querySelector("#productContainer");
+const productTemplate = document.querySelector("#productTemplate");
+
+export const showProductContainer = (products) => {
+  if (!products) {
+    return;
+  }
+  products.forEach((curProd) => {
+    const { id, name, category, brand, price, stock, image, description } =
+      curProd; //  Example : const id = curProd.id;
+
+    /* Template cloning process*/
+    const productClone = document.importNode(productTemplate.content, true); // we use 'true' to also import the children of parent node
+
+    /* Give card unique*/
+    productClone.querySelector('#cardValue').setAttribute('id' , `card${id}`);  //example : id = 'card1'
+
+
+    productClone.querySelector(".productName").textContent = name;
+    productClone.querySelector(".productImage").src = image;
+    productClone.querySelector(".category").textContent = category;
+    productClone.querySelector(".productPrice").textContent = `₹${price}`;
+    productClone.querySelector(".productActualPrice").textContent =
+      `₹${Math.floor(4 * price)}`;
+    productClone.querySelector(".productStock").textContent = stock;
+    productClone.querySelector(".productStock").textContent = stock;
+    productClone.querySelector(".productDescription").textContent = description;
+    productClone.querySelector(".productImage").alt = name;
+
+    /* Quantity Toggle*/
+    productClone
+      .querySelector(".stockElement")
+      .addEventListener("click", (e) => {
+        homeQuantityToggle(event, id, stock);
+      });
+
+    productContainer.append(productClone);
+  });
+};
---
+++ quantityToggle.js
@@ -0,0 +1,27 @@
+export const homeQuantityToggle = (event, id, stock) => {
+  // konsa card click kiys
+  const currentCardElement = document.querySelector(`#card${id}`);
+  // konse card ki quantity hae
+  const productQuantity = currentCardElement.querySelector(".productQuantity");
+
+  // Access the current card quantity
+  let quantity = parseInt(productQuantity.getAttribute("data-quantity")) || 1;
+
+  if (event.target.className === "cartIncrement") {
+    if (quantity < stock) {
+      quantity++;
+    } else if (quantity === stock) {
+      quantity = stock;
+    }
+  }
+
+  if (event.target.className === "cartDecrement") {
+    if (quantity > 1) {
+      quantity--;
+    }
+  }
+
+  productQuantity.textContent = quantity;
+  productQuantity.setAttribute("data-quantity", quantity);
+  return quantity;
+};
